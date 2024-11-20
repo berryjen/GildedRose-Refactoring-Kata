fdb97a07ff83f2f1e52487e958fdfc9108586602
Jennifer Su <jennifer.cc.su@gmail.com>
2024-11-20 11:35:59 +0100

    Squashed commit of the following:
    commit 7c7b360d2487021c82e349e824896aa2e869b49c
    Author: crvye <finn.schroeder@1komma5grad.com>
    Date:   Wed Nov 20 11:23:58 2024 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TypeScript/.tool-versions
    
    commit 9f439ac08c506d7b0bee1a51cbdd60a008294a89
    Author: Jennifer Su <jennifer.cc.su@gmail.com>
    Date:   Wed Nov 20 11:10:52 2024 +0100
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:TypeScript/test/jest/gilded-rose.spec.ts
    
    Co-authored-by: crvye <finn.schroeder@1komma5grad.com>

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -2,8 +2,8 @@ import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
   it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
+    const gildedRose = new GildedRose([new Item('Aged Brie', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('Aged Brie');
   });
 });
