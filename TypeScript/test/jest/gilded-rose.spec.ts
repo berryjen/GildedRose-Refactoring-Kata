80df4339d6b54edc46f0d999e5db04b5d7a3272b
crvye <finn.schroeder@1komma5grad.com>
2024-11-22 12:02:56 +0100

    test: added quality tests

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -6,4 +6,19 @@ describe('Gilded Rose', () => {
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('Aged Brie');
   });
+  it('should decrease quality of regular items by 1', () => {
+    const gildedRose = new GildedRose([new Item('Potatoes', 1, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+  it('should never decrease the quality below 0', () => {
+    const gildedRose = new GildedRose([new Item('Potatoes', 1, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+  });
+  it('should never lower the quality of Sulfuras', () => {
+    const gildedRose = new GildedRose([new Item('Sulfuras', 0, 1)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(1);
+  });
 });
